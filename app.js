8fa1208285bbb2f5b63e70b3c69a2c06496d77c2
Atul Rauthan <atulrauthan7@gmail.com>
2018-07-03 10:34:56 +0530

    changed to dynamic port

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
 	console.log("Yelp Camp Server has started!")
 });
